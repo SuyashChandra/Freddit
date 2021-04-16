5713229c83212b895213e440dc035affbb09be62
Suaysh Chandra <suyash.chandra24@gmail.com>
2021-04-16 15:52:53 +0530

    Reading data using Graphql

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,23 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  //We can choose not to expose the tilte in our GraphQL schema, by remmoving @Field. That way we still have the title in our database but not queryable by the GraphQL
+  @Field()
   @Property({ type: "text" })
   title!: string;
 }
